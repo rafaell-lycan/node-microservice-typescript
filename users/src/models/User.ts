07bcbadc192a02b00a03894ece9110525ba5ccd4
Rafaell Lycan <sonny.webdsg@gmail.com>
2020-04-01 23:35:29 +0200

    Add User model

---
+++ users/src/models/User.ts
@@ -0,0 +1,52 @@
+import * as bcrypt from 'bcrypt';
+import {Entity, PrimaryGeneratedColumn, Column, BeforeInsert} from 'typeorm'
+import { IsNotEmpty, IsEmail } from 'class-validator';
+import { Exclude } from 'class-transformer';
+
+@Entity()
+export class User {
+  static async hashPassword(password: string): Promise<string> {
+    return await bcrypt.hash(password, 10);
+  }
+
+  static async comparePassword(user: User, password: string): Promise<boolean> {
+    return await bcrypt.compare(password, user.password);
+  }
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @IsNotEmpty()
+  @Column({
+    name: 'first_name',
+    length: 128,
+  })
+  firstName!: string;
+
+  @IsNotEmpty()
+  @Column({
+    name: 'last_name',
+    length: 128,
+  })
+  lastName!: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @Column({
+    length: 128,
+    unique: true,
+  })
+  email!: string;
+
+  @IsNotEmpty()
+  @Column({
+    length: 80,
+  })
+  @Exclude({toPlainOnly: true})
+  password!: string;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    this.password = await User.hashPassword(this.password);
+  }
+}
